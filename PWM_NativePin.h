8b5ef6b8128e2df398564726f3426d338672251d
Alan Grover <awgrover@gmail.com>
2020-02-01 15:45:24 -0500

    add uno

--- PWM_NativePin.h
+++ PWM_NativePin.h
@@ -18,15 +18,24 @@ class PWM_NativePin {
     static void set(int pin, float state) { set(pin, (int) state); } // tolerate float
   };
 
+class PWM_NativePin_Uno : public PWM_NativePin {
+    public:
+
+      // pin names
+      // fixme: possible this should just be a constexpr array
+      enum {
+        pwm1=3, pwm2=5, pwm3=6, pwm4=9, pwm5=10, pwm6=11
+        };
+    };
+
 class PWM_NativePin_MEGA : public PWM_NativePin {
     public:
 
       // pin names
+      // fixme: possible this should just be a constexpr array
       enum {
         pwm1=2, pwm2, pwm3, pwm4, pwm5, pwm6, pwm7, pwm8, pwm9, pwm10, pwm11, pwm12, 
         pwm13=44, pwm14=45, pwm15=46
         };
-
-      //boolean begin(int pin) { PWM_NativePin::begin(pin); }
     };
 
